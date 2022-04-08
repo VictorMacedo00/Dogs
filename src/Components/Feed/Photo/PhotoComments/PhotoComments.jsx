d798cc3d091246e9530dfe1bfb77705d767e5cc9
Victor Macedo <joaovictormacedo.santos99@gmail.com>
2022-04-08 16:44:14 -0300

    Comments

--- src/Components/Feed/Photo/PhotoComments/PhotoComments.jsx
+++ src/Components/Feed/Photo/PhotoComments/PhotoComments.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import styles from "./PhotoComments.module.css";
+import { UserContext } from "./../../../../Contexts/UserContext";
+import PhotoCommentsForm from "./PhotoCommentsForm/PhotoCommentsForm";
 
-const PhotoComments = () => {
-  return <div>PhotoComments</div>;
+const PhotoComments = (props) => {
+  const { login } = useContext(UserContext);
+  const commentsSection = useRef();
+  const [comments, setComments] = useState(() => props.comments);
+
+  useEffect(() => {
+    commentsSection.current.scrollTop = comments.current.scrollHeight;
+  }, [comments]);
+
+  return (
+    <>
+      <ul ref={commentsSection} className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
+  );
 };
 
 export default PhotoComments;
