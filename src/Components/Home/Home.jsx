c2091694bb1e4fcde0092ce1905996ba8ac4a5c0
Victor Macedo <joaovictormacedo.santos99@gmail.com>
2022-04-08 18:37:53 -0300

    Loading animation

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Home.module.css";
 import Feed from "./../Feed/Feed";
+import Loading from "../Helper/Loading";
 
 export const Home = () => {
   return (
